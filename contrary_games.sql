31f8bd4359aea034b4b7dda2d7750423561c8b3d
Bryce Kerley <bkerley@brycekerley.net>
2021-07-27 22:52:49 -0400

    count up contrary points

--- contrary_games.sql
+++ contrary_games.sql
@@ -14,7 +14,8 @@ with
                                         when day >= 27 and day <= 71 then 2
                                         when day >= 72 and day <= 98 then 3
                                         when day >= 99 then 100
-                                        else 666 end) as phase_num
+                                        else 666 end) as phase_num,
+                    round(greatest(home_odds, away_odds) * 100, 0) as max_odds
                         from data.games
                 where winning_pitcher_id is not null),
         rounded_games as (
@@ -22,17 +23,21 @@ with
                         day, season, phase, phase_num,
                         round(home_odds * 100, 0) as home_odds,
                         round(away_odds * 100, 0) as away_odds,
-                        round(greatest(home_odds, away_odds) * 100, 0) as max_odds,
+                                                max_odds,
                         home_score, away_score,
                         contrary,
-                        (case when contrary then 1 else 0 end) as contrary_num
+                        (case when contrary then 1 else 0 end) as contrary_num,
+                                                (case
+                                                        when contrary then max_odds - 50
+                                                        else 0 end) as contrary_points
                 from finished_games
         )
 select
-        season, phase,
+        phase,
         count(day) as total_games,
         sum(contrary_num) as contrary_games,
-                round(100.0 * sum(contrary_num) / count(day), 0) as contrary_pct
+        round(100.0 * sum(contrary_num) / count(day), 0) as contrary_pct,
+                sum(contrary_points) as total_contrary_points
         from rounded_games
-        group by season, phase, phase_num
-        order by season asc, phase_num asc
+        group by phase, phase_num
+        order by phase_num asc
